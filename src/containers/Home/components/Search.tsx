d716f84f0ad2d1cd0e70e8c8cf9f3eed61b532a5
bazzomir <bazzomir97@gmail.com>
2025-12-31 06:03:28 +0100

    updated component

--- src/containers/Home/components/Search.tsx
+++ src/containers/Home/components/Search.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import {SearchBar} from '../../../components/RightSide/SearchBar';
-import {BsSearch} from 'react-icons/bs';
-import {RiSettings2Line} from 'react-icons/ri';
-import {Styled} from './Navbar';
-import {WhoToFollow} from '../../../components/RightSide/WhoToFollow';
-import {TrendsForYous} from '../../../components/RightSide/TrendsForYou';
-import {RightBottomText} from '../../../components/RightSide/RightSideItems/RightSideItems';
+import { SearchBar } from '../../../components/RightSide/SearchBar';
+import { BsSearch } from 'react-icons/bs';
+import { RiSettings2Line } from 'react-icons/ri';
+// import { Styled } from '../../Navbar';
+import styled from 'styled-components';
+import { WhoToFollow } from '../../../components/RightSide/WhoToFollow';
+import { TrendsForYous } from '../../../components/RightSide/TrendsForYou';
+// import { RightBottomText } from '../../../components/RightSide/RightSideItems/RightSideItems';
 
 export const Search = () => {
   return (
@@ -13,7 +14,15 @@ export const Search = () => {
       <SearchBar icon={<BsSearch />} />
       <TrendsForYous text="Trends for you" icon={<RiSettings2Line />} />
       <WhoToFollow text="Who to follow" />
-      <RightBottomText />
+      {/* <RightBottomText /> */}
     </Styled.Container>
   );
+};
+
+const Styled = {
+  Container: styled.div`
+    background: black;
+    color: whitesmoke;
+    border-left: 1px solid white;
+  `,
 };
