7ddcf4d2babe8c3a18b1de6688734939f5ab7e2a
bazzomir <bazzomir97@gmail.com>
2025-12-31 18:27:06 +0100

    bug fixed, clean code

--- src/mockApi/login.ts
+++ src/mockApi/login.ts
@@ -1,5 +1,5 @@
-import {LocalStorageConstants} from '../constants/constants';
-import {readFromStorage, writeInStorage} from '../utils/localStorage';
+import { LocalStorageConstants } from '../constants/constants';
+import { readFromStorage, writeInStorage } from '../utils/localStorage';
 
 export class CustomLoginError extends Error {
   status: any;
@@ -8,8 +8,6 @@ export class CustomLoginError extends Error {
     this.message = message;
   }
 }
-
-const nekojaGreshka = new CustomLoginError('Check your credentials');
 
 interface User {
   email: string;
@@ -22,13 +20,12 @@ export const loginApi = ({
 }: {
   email: string;
   password: string;
-}): Promise<{accessToken: string; refreshToken: string; email: string; password: string}> => {
+}): Promise<{ accessToken: string; refreshToken: string; email: string; password: string }> => {
   return new Promise((resolve, reject) => {
-    if (password.length < 2) {
-      reject(new CustomLoginError('Plase make your password at least 2 characters'));
+    if (password.length < 3) {
+      reject(new CustomLoginError('Plase make your password at least 3 characters'));
     }
     var validatedPassword = readFromStorage(email);
-    //if validatedPassword is null - the user does not exist
     if (!!validatedPassword) {
       resolve({
         accessToken: LocalStorageConstants.AccessToken,
@@ -37,13 +34,12 @@ export const loginApi = ({
         password: validatedPassword,
       });
     } else {
-      //   reject('Check your email and password.');
       reject(new CustomLoginError('Check your email and password.', 401));
     }
   });
 };
 
-export const logoutApi = ({accessToken}: {accessToken: string | null}): Promise<boolean> => {
+export const logoutApi = ({ accessToken }: { accessToken: string | null }): Promise<boolean> => {
   return new Promise((resolve, reject) => {
     if (accessToken) {
       resolve(true);
@@ -57,13 +53,13 @@ export const getUserApi = ({
   accessToken,
 }: {
   accessToken: string | null;
-}): Promise<{email: string; password: string}> => {
+}): Promise<{ email: string; password: string }> => {
   return new Promise((resolve, reject) => {
     if (accessToken) {
       const email = readFromStorage(LocalStorageConstants.Email);
       const password = readFromStorage(LocalStorageConstants.Password);
       if (email != null && password != null) {
-        resolve({email: email, password: password});
+        resolve({ email: email, password: password });
       }
     } else {
       reject('You must login');
@@ -82,12 +78,12 @@ export const registerUserApi = ({
   birthday: string;
   password: string;
   repeatPassword: string;
-}): Promise<{user: User; accessToken: string; refreshToken: string}> => {
+}): Promise<{ user: User; accessToken: string; refreshToken: string }> => {
   return new Promise((resolve, reject) => {
     if (password === repeatPassword) {
       writeInStorage(email, password);
       resolve({
-        user: {email, password},
+        user: { email, password },
         accessToken: LocalStorageConstants.AccessToken,
         refreshToken: LocalStorageConstants.RefreshToken,
       });
