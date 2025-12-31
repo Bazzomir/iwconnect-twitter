0fed395f238bbb8c3acf56d1d6b8d0708e0decfb
bazzomir <bazzomir97@gmail.com>
2025-12-31 18:30:15 +0100

    css issue fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import './index.css';
+import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
-import {CustomThemeContextConstructor} from './context/CustomThemeContext';
-import {TweetsContextConstructor} from './context/TweetsContext';
-import {TweetsCommentsContextConstructor} from './context/TweetsCommentsContext';
-import {AuthContextConstructor} from './context/AuthContext';
-import {store} from './state/store';
+import { BrowserRouter } from 'react-router-dom';
+import { CustomThemeContextConstructor } from './context/CustomThemeContext';
+import { TweetsContextConstructor } from './context/TweetsContext';
+import { TweetsCommentsContextConstructor } from './context/TweetsCommentsContext';
+import { AuthContextConstructor } from './context/AuthContext';
+import { store } from './state/store';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,11 +17,11 @@ ReactDOM.render(
       <AuthContextConstructor>
         <BrowserRouter>
           <CustomThemeContextConstructor>
-          <TweetsContextConstructor>
-            <TweetsCommentsContextConstructor>
-              <App />
-            </TweetsCommentsContextConstructor>
-          </TweetsContextConstructor>
+            <TweetsContextConstructor>
+              <TweetsCommentsContextConstructor>
+                <App />
+              </TweetsCommentsContextConstructor>
+            </TweetsContextConstructor>
           </CustomThemeContextConstructor>
         </BrowserRouter>
       </AuthContextConstructor>
