bdcb2ae3d9c22907bdce9c33be988f697fa2be3d
bazzomir <bazzomir97@gmail.com>
2026-01-25 04:10:02 +0100

    responsive main page

--- src/containers/Home/components/Main/Main.tsx
+++ src/containers/Home/components/Main/Main.tsx
@@ -1,15 +1,15 @@
-import React, {useContext} from 'react';
-import {AddTweet} from './components/AddTweet/AddTweet';
-import {Post} from './components/Post/Post';
-import {TweetsContext} from '../../../../context/TweetsContext';
-import {PageWrapperComponent} from '../../../PageWrapper/PageWrapperComponent';
+import React, { useContext } from 'react';
+import { AddTweet } from './components/AddTweet/AddTweet';
+import { Post } from './components/Post/Post';
+import { TweetsContext } from '../../../../context/TweetsContext';
+import { PageWrapperComponent } from '../../../PageWrapper/PageWrapperComponent';
 
 export const Main = () => {
-  const {tweets} = useContext(TweetsContext);
+  const { tweets } = useContext(TweetsContext);
 
   return (
     <PageWrapperComponent>
-      <main className="col-6">
+      <main className="col-12 col-md-6">
         <AddTweet />
         {tweets?.slice(0, 10)?.map(post => {
           return <Post key={post.id} {...post} />;
