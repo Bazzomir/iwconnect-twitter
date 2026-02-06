6a303941e3c3e04bc4010f0a5c120b6ad183af27
bazzomir <bazzomir97@gmail.com>
2026-02-06 02:37:49 +0100

    chage name from "itemsByPostId" to "commentsByPostId"

--- src/state/comments/comments.selector.ts
+++ src/state/comments/comments.selector.ts
@@ -1,5 +1,5 @@
 import { RootState } from '../store';
 
-export const selectCommentsByPostId = (postId: number) => (state: RootState) => state.comments.itemsByPostId[postId] || [];
+export const selectCommentsByPostId = (postId: number) => (state: RootState) => state.comments.commentsByPostId[postId] || [];
 export const selectCommentsLoading = (state: RootState) => state.comments.loading;
 export const selectCommentsError = (state: RootState) => state.comments.error;
